dc9cac41139ff33a5e08a4ccd883d84642e9927a
AleFeri <50620354+AleFeri@users.noreply.github.com>
2020-11-29 21:58:22 +0100

    Tetativo fallito con XML

--- src/PuntoVendita.java
+++ src/PuntoVendita.java
@@ -1,3 +1,9 @@
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class PuntoVendita {
     private int idPuntoVendita;
     private String denominazione;
--- src/XMLHandler.java
+++ src/XMLHandler.java
@@ -1,14 +1,41 @@
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.File;
 import java.util.List;
 
 public class XMLHandler {
 
+    private JAXBContext context;
+
+    public XMLHandler() throws JAXBException {
+        this.context = JAXBContext.newInstance(PuntoVendita.class);
+    }
+
     //Metodi
-    public static void JSONToXML(List<PuntoVendita> puntiVenditaList) {
-        
+    public void JSONToXML(List<PuntoVendita> puntiVenditaList) {
+        File xmlFile = new File("puntiVendita.xml");
+
+        try {
+            Marshaller marshaller = context.createMarshaller();
+
+            for(PuntoVendita pv: puntiVenditaList) {
+                marshaller.marshal(pv, xmlFile);
+            }
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
+        new JSONHandler();
+        List<PuntoVendita> pv = JSONHandler.getPuntiVenditaArray();
 
+        try {
+            XMLHandler xmlHandler = new XMLHandler();
+            xmlHandler.JSONToXML(pv);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
     }
 }
